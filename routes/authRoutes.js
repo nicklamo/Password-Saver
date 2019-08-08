d721fa827069328a8500765c9ebdce33370ad95b
Nick <nila2238@colorado.edu>
2019-08-08 17:43:25 -0600

    will this ever work

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,21 +9,21 @@ const middleware = require('../middleware');
 // ====================
 
 //show register form
-router.get('/register', middleware.checkNotAutheniticated ,(req,res) => {
-    res.render('register',{currentUser: req.user});
+router.get('/register', middleware.checkNotAutheniticated, (req, res) => {
+    res.render('register', { currentUser: req.user });
 });
 //register a user
-router.post('/register', middleware.checkNotAutheniticated ,async (req,res) => {
-    try{ //dont register if the password is not equal
-        if(req.body.password !== req.body.password_conf){
+router.post('/register', middleware.checkNotAutheniticated, async (req, res) => {
+    try { //dont register if the password is not equal
+        if (req.body.password !== req.body.password_conf) {
             res.redirect('/register');
         }
         //store user in database
-        const newUser = await new User({username: req.body.email});
-        const registered = await User.register(newUser, req.body.password);
+        const newUser = User({ username: req.body.email });
+        User.register(newUser, req.body.password);
         passport.authenticate('local');
         res.redirect('/login');
-    } catch(e){
+    } catch (e) {
         console.log(e.message);
         res.redirect('/register');
     }
@@ -34,18 +34,18 @@ router.post('/register', middleware.checkNotAutheniticated ,async (req,res) => {
 // =============
 
 //show login form
-router.get('/login', middleware.checkNotAutheniticated ,(req,res) =>{
-    res.render('login', {currentUser: req.user})
+router.get('/login', middleware.checkNotAutheniticated, (req, res) => {
+    res.render('login', { currentUser: req.user })
 });
 //login a user
-router.post('/login',passport.authenticate('local', {
+router.post('/login', passport.authenticate('local', {
     successRedirect: '/passwords',
     failureRedirect: '/',
     failureFlash: true
 }));
 
 //logout a user
-router.delete('/logout', (req,res) => {
+router.delete('/logout', (req, res) => {
     req.logOut();
     res.redirect('/login');
 });
